ab2a81bf46fb27c51e44df76d884ffcf909fb1a6
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-03-23 13:36:15 +0330

    🗑 deleting search app

--- divar/search/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from search.models import Users
-
-admin.site.register(Users)
--- divar/search/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from search.views import show_products
-
-urlpatterns = [
-    path('<str:city>/<str:category>', show_products)
-]
