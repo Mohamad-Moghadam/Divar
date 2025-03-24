c29afea495110a665b2bb6b2e2edec0e376a72d1
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-03-24 20:58:41 +0330

    ✨ adding user view added

--- divar/user/views.py
+++ divar/user/views.py
@@ -1,2 +1,13 @@
 from django.shortcuts import render
+import json
+from user.models import User
 
+def add_user(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+
+        User.objects.create(
+            name = data.get("name"),
+            number = data.get("number"),
+            national_id_number = data.get("national_id_number"),
+        )
