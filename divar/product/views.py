b8eb0e5bd99f69b981ae924b7c85eba71aa8f8be
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-03-24 20:25:22 +0330

    ✨ add product view added

--- divar/product/views.py
+++ divar/product/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from product.models import Product
+import json
+from product.models import Product
 
 def show_all_prducts(request):
     products = Product.objects.all()
     return render(request , 'product/product.html', {'products': products})
+
+def add_product(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+
+        Product.objects.create(
+            seller_id = data.get('seller'),
+            name = data.get('name'),
+            location = data.get('location'),
+        )
