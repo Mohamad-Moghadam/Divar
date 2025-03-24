be50a7328d823625edc4555f970ca6a0e65346e0
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-03-24 21:03:24 +0330

    ✨ adding user url added

--- divar/user/urls.py
+++ divar/user/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from user.views import add_user
+
 
 urlpatterns = [
-    
+    path('sign-up', add_user)
 ]
--- divar/user/views.py
+++ divar/user/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 import json
 from user.models import User
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
+
+@csrf_exempt
 def add_user(request):
     if request.method == 'POST':
         data = json.loads(request.body)
@@ -11,3 +15,5 @@ def add_user(request):
             number = data.get("number"),
             national_id_number = data.get("national_id_number"),
         )
+
+        return HttpResponse(f"{data.get("name")} was added as a user")
